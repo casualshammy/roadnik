3773751d1de1a4178fcd8d958a43089dd377cce3
casualshammy <axio@axio.name>
2024-07-29 18:18:48 +0100

    nuget update

--- Roadnik/Interfaces/ITilesCache.cs
+++ Roadnik/Interfaces/ITilesCache.cs
@@ -5,6 +5,5 @@ namespace Roadnik.Interfaces;
 public interface ITilesCache
 {
   void EnqueueUrl(int _x, int _y, int _z, string _type, string _url);
-  Stream? GetOrDefault(int _x, int _y, int _z, string _type);
   bool TryGet(int _x, int _y, int _z, string _type, [NotNullWhen(true)] out Stream? _stream, [NotNullWhen(true)] out string? _hash);
 }

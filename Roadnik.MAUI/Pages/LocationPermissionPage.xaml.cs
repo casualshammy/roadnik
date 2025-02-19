cf0280c28127d72d07aa2847d6c34181f99378cc
casualshammy <axio@axio.name>
2025-02-19 20:32:24 +0300

    up(pages): added `location permission` and `settings error` pages

--- Roadnik.MAUI/Pages/LocationPermissionPage.xaml.cs
+++ Roadnik.MAUI/Pages/LocationPermissionPage.xaml.cs
@@ -1,18 +1,58 @@
+using L = Roadnik.MAUI.Resources.Strings.AppResources;
+
 namespace Roadnik.MAUI.Pages;
 
 public partial class LocationPermissionPage : ContentPage
 {
-	public LocationPermissionPage()
-	{
-		InitializeComponent();
-		BindingContext = this;
-	}
+  private readonly bool p_permissionAlways;
+  private readonly Func<bool, Task> p_onClose;
 
-  public string LocationPermissionDescription { get; } =
-    "Our app requires access to your device's background location in order to provide you with accurate and up-to-date location tracking. " +
-    "This means that even when our app is not shown on the screen, we're still able to keep track of your location in the background if you'll switch on the share function.\r\n\r\n" +
-    "We understand that privacy is important to you, and we want to assure you that we take the protection of your personal data seriously. " +
-    "Rest assured that we will only collect and use your location information in accordance with our privacy policy, " +
-    "which is designed to protect your data and ensure that it is only used for the purposes you have explicitly consented to.\r\n\r\n" +
-    "If you'll press 'Okay' button, you'll be redirected to app's settings page, where you should select 'Allow all the time' for location permission";
+  public LocationPermissionPage(
+    bool _permissionAlways,
+    Func<bool, Task> _onClose)
+	{
+    p_permissionAlways = _permissionAlways;
+    p_onClose = _onClose;
+
+    if (_permissionAlways)
+      DescriptionText = L.page_location_permission_always;
+    else
+      DescriptionText = L.page_location_permission_app_in_use;
+
+    OkButtonText = L.page_location_permission_ok_btn;
+    CancelButtonText = L.page_location_permission_cancel_btn;
+
+    InitializeComponent();
+    BindingContext = this;
+  }
+
+  public string DescriptionText { get; }
+  public string OkButtonText { get; }
+  public string CancelButtonText { get; }
+
+  private async void OnOkButtonClicked(object _s, EventArgs _e)
+  {
+    if (p_permissionAlways)
+    {
+      var osVersion = DeviceInfo.Current.Version;
+      if (osVersion.Major < 11)
+        await Permissions.RequestAsync<Permissions.LocationAlways>();
+      else
+        AppInfo.Current.ShowSettingsUI();
+    }
+    else
+    {
+      await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+    }
+
+    await p_onClose(true);
+    await Navigation.PopModalAsync(true);
+  }
+
+  private async void OnCancelButtonClicked(object _s, EventArgs _e)
+  {
+    await p_onClose(false);
+    await Navigation.PopModalAsync(true);
+  }
+
 }

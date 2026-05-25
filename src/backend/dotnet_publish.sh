642c468469bb62c1e361240c2da8c790479038a7
casualshammy <axio@axio.name>
2026-05-25 19:30:07 +0300

    build(server): alpine build (5)

--- src/backend/dotnet_publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGETARCH=$1
-
-if [ "$TARGETARCH" == "amd64" ]; then
-    dotnet publish -r linux-musl-x64 -o out src/backend && chmod +x /app/out/roadnik-server
-elif [ "$TARGETARCH" == "arm64" ]; then
-    dotnet publish -r linux-musl-arm64 -o out src/backend && chmod +x /app/out/roadnik-server
-fi
